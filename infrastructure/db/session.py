f11b3cfcdfdaa6d6c85b84f6deb90542f3464024
Aleksandra Rumiantseva <alexrumyantceva@gmail.com>
2026-04-15 18:49:37 +0300

    fix deploy.yml

--- infrastructure/db/session.py
+++ infrastructure/db/session.py
@@ -2,7 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ["POSTGRES_CONNECTION_STRING"].replace(
+    "postgres://", "postgresql://"
+)
 
 engine = create_engine(DATABASE_URL)
 
